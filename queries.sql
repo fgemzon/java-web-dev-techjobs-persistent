d76c43833efb16cf22ccca27765ebf720dce7c61
Fionna <fionnagemzon@gmail.com>
2021-03-24 17:13:55 -0500

    displayViewEmployer

--- queries.sql
+++ queries.sql
@@ -1,6 +1,7 @@
 ## Part 1: Test it with SQL
 
 ## Part 2: Test it with SQL
+SELECT name from employer WHERE location = "St. Louis City";
 
 ## Part 3: Test it with SQL
 
---
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/data/SkillRepository.java
@@ -0,0 +1,13 @@
+package org.launchcode.javawebdevtechjobspersistent.models.data;
+
+import org.launchcode.javawebdevtechjobspersistent.models.Skill;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SkillRepository extends CrudRepository <Skill, Integer>{
+
+    /* Optional findByID(int skillId);*/
+}
